57b720a07dc6ad86c7ce2a89feb8417d167cd96e
Nitin srivastva <nitinsrivastava2all@gmail.com>
2023-05-09 11:53:30 +0530

    My-Home component

--- contracts/MusicNFTMarketplace.sol
+++ contracts/MusicNFTMarketplace.sol
@@ -33,7 +33,6 @@ contract MusicNFTMarketplace is ERC721("DAppFi","DAPP"), Ownable
         uint256 price
     );
 
-    /* In constructor we initalize royalty fee, artist address and prices of music nfts*/
     constructor(
         uint256 _royaltyFee,
         address _artist,
@@ -52,13 +51,10 @@ contract MusicNFTMarketplace is ERC721("DAppFi","DAPP"), Ownable
         }
     }
 
-    /* Updates the royalty fee of the contract */
     function updateRoyaltyFee(uint256 _royaltyFee) external onlyOwner {
         royaltyFee = _royaltyFee;
     }
 
-    /* Creates the sale of a music nft listed on the marketplace */
-    /* Transfers ownership of the nft, as well as funds between parties */
     function buyToken(uint256 _tokenId) external payable {
         uint256 price = marketItems[_tokenId].price;
         address seller = marketItems[_tokenId].seller;
@@ -73,7 +69,6 @@ contract MusicNFTMarketplace is ERC721("DAppFi","DAPP"), Ownable
         emit MarketItemBought(_tokenId, seller, msg.sender, price);
     }
 
-    /* Allows someone to resell their music nft */
     function resellToken(uint256 _tokenId, uint256 _price) external payable {
         require(msg.value == royaltyFee, "Must pay royalty");
         require(_price > 0, "Price must be greater than zero");
@@ -84,7 +79,6 @@ contract MusicNFTMarketplace is ERC721("DAppFi","DAPP"), Ownable
         emit MarketItemRelisted(_tokenId, msg.sender, _price);
     }
 
-    /* Fetches all the tokens currently listed for sale */
     function getAllUnsoldTokens() external view returns (MarketItem[] memory) {
         uint256 unsoldCount = balanceOf(address(this));
         MarketItem[] memory tokens = new MarketItem[](unsoldCount);
@@ -98,7 +92,6 @@ contract MusicNFTMarketplace is ERC721("DAppFi","DAPP"), Ownable
         return (tokens);
     }
 
-    /* Fetches all the tokens owned by the user */
     function getMyTokens() external view returns (MarketItem[] memory) {
         uint256 myTokenCount = balanceOf(msg.sender);
         MarketItem[] memory tokens = new MarketItem[](myTokenCount);
@@ -112,7 +105,6 @@ contract MusicNFTMarketplace is ERC721("DAppFi","DAPP"), Ownable
         return (tokens);
     }
 
-    /* Internal function that gets the baseURI initialized in the constructor */
     function _baseURI() internal view virtual override returns (string memory) {
         return baseURI;
     }
